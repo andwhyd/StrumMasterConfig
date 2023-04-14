dcf854f80b0edd5ee023452c624488cebd2eb17f
andwhyd <33667699+andwhyd@users.noreply.github.com>
2023-04-14 06:43:02 -0400

    add keyboard support to live mode

--- src/lib/InputCard.svelte
+++ src/lib/InputCard.svelte
@@ -15,6 +15,18 @@
     import { selMode } from "../store.js";
 
     export let idx = 0;
+    export let keybind = "" + (idx + 1);
+    switch (idx) {
+        case 10 - 1:
+            keybind = "0";
+            break;
+        case 11 - 1:
+            keybind = "-";
+            break;
+        case 12 - 1:
+            keybind = "=";
+            break;
+    }
 
     onMount(() => {
         write_config($currentConfig.buttonConfigs[idx]);
@@ -45,7 +57,15 @@
             selStrings[j] = (configInt >> j) & 0b1 ? true : false;
         }
     };
+
+    const handleKeydown = (event) => {
+        if ($selMode == modes.live && event.key === keybind) {
+            dispatch("livePlay", { id: idx });
+        }
+    };
 </script>
+
+<svelte:window on:keydown={handleKeydown} />
 
 <div class="input_card" transition:fade={{ duration: 100 }}>
     <div>
@@ -59,8 +79,8 @@
                 </option>
             {/each}
         </select>
-        {#each strings as string, j}
-            <div class="checkbox">
+        <div class="checkbox">
+            {#each strings as string, j}
                 <label>
                     <input
                         type="checkbox"
@@ -69,14 +89,15 @@
                     />
                     {"String " + string}
                 </label>
-            </div>
-        {/each}
+            {/each}
+        </div>
     </div>
     {#if $selMode == modes.live}
         <div class="live_control" transition:slide>
             <button on:click={() => dispatch("livePlay", { id: idx })}>
                 Play
             </button>
+            <button class="kbd">{keybind}</button>
         </div>
     {/if}
 </div>
@@ -90,25 +111,56 @@
         text-align: left;
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
         transition: 0.3s;
+        min-width: 12vw;
     }
     .input_card:hover {
-        box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+        box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.4);
     }
     .card_header {
         text-align: center;
         margin: 0 0 1rem;
     }
     .live_control {
+        display: flex;
+        justify-content: space-evenly;
         margin: 1rem 0rem 0.5rem;
         text-align: center;
     }
     select {
+        border-radius: 0.8rem;
         padding: 0.5rem;
         margin-bottom: 1rem;
         font-size: large;
+        width: 100%;
     }
     .checkbox {
+        border-radius: 0.8rem;
+        border-bottom: 1px solid gray;
+    }
+    .checkbox > label:first-child {
+        border-top-left-radius: 0.8rem;
+        border-top-right-radius: 0.8rem;
+    }
+    .checkbox > label:last-child {
+        border-bottom-left-radius: 0.8rem;
+        border-bottom-right-radius: 0.8rem;
+    }
+    label {
+        display: block;
         font-size: large;
         padding: 0.5rem;
+        border: 1px solid gray;
+        border-bottom: 0px;
+    }
+    .kbd {
+        background-color: #eee;
+        color: #555;
+        border-radius: 0.1rem;
+        padding: 0.2rem 0.6rem;
+        border-top: 0.5rem solid rgba(255, 255, 255, 0.5);
+        border-left: 0.5rem solid rgba(255, 255, 255, 0.5);
+        border-right: 0.5rem solid rgba(0, 0, 0, 0.2);
+        border-bottom: 0.5rem solid rgba(0, 0, 0, 0.2);
+        text-align: center;
     }
 </style>
